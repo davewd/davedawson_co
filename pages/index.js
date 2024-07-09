4eca8e68dc1c72b2dd497addb6d3915d99421cf2
David Dawson <davedawson.co@gmail.com>
2024-07-09 10:44:25 +1000

    fix(intro) remove incorrect syntax

--- pages/index.js
+++ pages/index.js
@@ -15,11 +15,9 @@ export default function Home() {
     br div className = "z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex" >
     <
     br p className = "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
-    Hi,
-    I 'm Dave. <br/>
-    I 've got XX Years experience at the intersection of tech and finance.<br/>
-    I build digital models of the real world to enable automation and prediction. < br / >
-    Take a look at some of my work below < /p > <
+    Hi, I 'm Dave. 
+    I 've got XX Years experience at the intersection of tech and finance.
+    I build digital models of the real world to enable automation and prediction.Take a look at some of my work below < /p > <
     div className = "fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none" >
     <
     a className = "pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
