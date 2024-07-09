9dfe8973c23c2954613e33821d92c40748e40d07
David Dawson <davedawson.co@gmail.com>
2024-07-09 10:46:49 +1000

    fix() Commit lint

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,13 @@ const inter = Inter({
 });
 
 export default function Home() {
-  return ( < br main className = {
+  return ( < main className = {
       `flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`
     } >
     <
-    br div className = "z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex" >
+    div className = "z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex" >
     <
-    br p className = "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
+    p className = "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
     Hi, I 'm Dave. 
     I 've got XX Years experience at the intersection of tech and finance.
     I build digital models of the real world to enable automation and prediction.Take a look at some of my work below < /p > <
