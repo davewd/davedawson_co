a2e69fa7c9cd6cd7d10cb4dfa6cbaa561eb08020
David Dawson <davedawson.co@gmail.com>
2024-07-09 10:53:25 +1000

    feat(intro) try to fix this formatting

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     div className = "z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex" >
     <
     p className = "fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" >
-    Hi, I 'm Dave. 
+    Hi, I 'm Dave.
     I 've got XX Years experience at the intersection of tech and finance.
     I build digital models of the real world to enable automation and prediction.Take a look at some of my work below < /p > <
     div className = "fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none" >
