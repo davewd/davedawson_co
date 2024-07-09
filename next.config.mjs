ae534edb67011084e03fef873c178a52f31c50c2
David Dawson <davedawson.co@gmail.com>
2024-07-09 13:45:15 +1000

    reformat

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/davedawson_co", // <=== enables base path for the app
-  assetPrefix: "/davedawson_co",
-  output: "export", // <=== enables static exports
+  basePath: '/davedawson_co', // <=== enables base path for the app
+  assetPrefix: '/davedawson_co',
+  output: 'export', // <=== enables static exports
   images: {
     unoptimized: true,
   },
